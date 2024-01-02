14977afc51ebedf1a4bf36f2e174998a07e2b881
Orkun Selçuk <yasino@Orkuns-MacBook-Pro.local>
2024-01-02 15:24:49 -0400

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home'
 import Info from './components/Info'
 import Menu from './components/Menu'
 import Gallery from './components/Gallery'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Info/>
       <Menu/>
       <Gallery/>
+      <Footer/>
     </div>
 
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+
+        
+        <div className='footer-left'>
+            <p> © 2024 Coffeeology Espresso Co.      |    <a>Contact</a> </p>
+
+
+        </div>
+
+
+        <div className='footer-right'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
